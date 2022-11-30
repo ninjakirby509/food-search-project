bd07c1733f082343acc11f1938d00d1b400b43dc
ChuongDinhVu <113396073+ChuongDinhVu@users.noreply.github.com>
2022-11-30 17:02:04 -0600

    basic flow from login to index
    The database has been set up, CSS is a work in process

--- food-search.py
+++ food-search.py
@@ -8,11 +8,18 @@ from flask_login import LoginManager
 
 app = flask.Flask(__name__)
 app.secret_key= "My key"
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+#app.config['SECRET_KEY'] = 'SUPERSECRETKEY'
+#yelp_db = SQLAlchemy(app)
 
 @app.route('/')
 def login():
     """The login in screen """
     return flask.render_template('login.html')
+@app.route('/login_handler',methods = ['POST'])
+def login_handler():
+    return flask.render_template('index.html')
+
 
 
 app.run(debug=True)
