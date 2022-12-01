31e5e904c82c61daaed582e5181cfe338c6ffc24
ChuongDinhVu <113396073+ChuongDinhVu@users.noreply.github.com>
2022-11-30 23:16:18 -0600

    Add location button on main page

--- food-search.py
+++ food-search.py
@@ -6,12 +6,31 @@ from dotenv import load_dotenv,find_dotenv
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
+load_dotenv(find_dotenv())
 app = flask.Flask(__name__)
+<<<<<<< Updated upstream
 app.secret_key= "My key"
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 #app.config['SECRET_KEY'] = 'SUPERSECRETKEY'
 #yelp_db = SQLAlchemy(app)
+=======
+yelp_url = "https://api.yelp.com/v3"
+search_url = "/businesses/search"
+yelp_key = os.getenv("YELP_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("LOCAL_DATABASE_URL")
+app.config['SECRET_KEY'] = 'SUPERSECRETKEY'
+yelp_db = SQLAlchemy(app)
+>>>>>>> Stashed changes
 
+class Person(yelp_db.Model):
+    __tablename__ = 'Username'
+    username = yelp_db.Column(yelp_db.String(50),primary_key = True)
+    password = yelp_db.Column(yelp_db.String(50),unique = True)
+    def __init__(self,username,password):
+        self.username = username
+        self.password = password
+with app.app_context():
+    yelp_db.create_all()
 @app.route('/')
 def login():
     """The login in screen """
@@ -22,6 +41,14 @@ def signup_handler():
     form_data = flask.request.form
     username = form_data["Username"]
     password = form_data["password"]
+    thisuser = Person.query.filter_by(username=username).first()
+    if thisuser is None:
+        print("User does not exist")
+        thisuser = Person(username,password)
+        yelp_db.session.add(thisuser)
+        yelp_db.session.commit()
+    else:
+        print("User Exit")
     return flask.render_template('login.html')
 @app.route('/login_handler',methods = ['POST'])
 def login_handler():
@@ -30,7 +57,33 @@ def login_handler():
     form_data = flask.request.form
     username = form_data["Username"]
     password = form_data["password"]
+<<<<<<< Updated upstream
     return flask.render_template('index.html')
+=======
+    thisuser = Person.query.filter_by(username=username).first()
+    if thisuser is None:
+        return flask.redirect(flask.url_for("login_handler"))
+    else:
+        return flask.redirect(flask.url_for("homepage"))
+@app.route('/index')
+def homepage():
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {yelp_key}"
+    }
+    params = {
+        "location": "Hallettsville",
+        "term": "food",
+        "price": [1,2,3,4],
+        "sort_by": "distance"
+    }
+    response = requests.get(yelp_url+search_url, headers=headers, params=params)
+    for business in response.json()["businesses"]:
+        print("name:", business['name'])
+        print("id:", business['id'])
+        print("url:", business['url'])
+    return flask.render_template('index.html', businesses = response.json()["businesses"])
+>>>>>>> Stashed changes
 
 
 app.run(debug=True)
