ffec405db52fa9f711e5aee7f8cc381037a5c642
Luke Renner <ninjakirby509@gmail.com>
2022-11-30 21:55:29 -0600

    Init Commit Dev

--- food-search.py
+++ food-search.py
@@ -7,9 +7,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = flask.Flask(__name__)
-app.secret_key= "My key"
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-#app.config['SECRET_KEY'] = 'SUPERSECRETKEY'
+yelp_url = "https://api.yelp.com/v3"
+search_url = "/businesses/search"
+yelp_key = os.getenv("YELP_KEY")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SECRET_KEY'] = 'SUPERSECRETKEY'
 #yelp_db = SQLAlchemy(app)
 
 @app.route('/')
@@ -23,7 +25,9 @@ def signup_handler():
     username = form_data["Username"]
     password = form_data["password"]
     return flask.render_template('login.html')
-@app.route('/login_handler',methods = ['POST'])
+
+
+@app.route('/login_handler', methods = ['POST'])
 def login_handler():
     """Handle Login"""
     print("Handle func")
@@ -31,6 +35,26 @@ def login_handler():
     username = form_data["Username"]
     password = form_data["password"]
     return flask.render_template('index.html')
+
+
+@app.route('/index')
+def homepage():
+    headers = {
+        "accept": "application/json",
+        "Authorization": f"Bearer {yelp_key}"
+    }
+    params = {
+        "location": "Hallettsville",
+        "term": "food",
+        "price": [1,2,3,4],
+        "sort_by": "distance"
+    }
+    response = requests.get(yelp_url+search_url, headers=headers, params=params)
+    for business in response.json()["businesses"]:
+        print("name:", business['name'])
+        print("id:", business['id'])
+        print("url:", business['url'])
+    return flask.render_template('index.html', businesses = response.json()["businesses"])
 
 
 app.run(debug=True)
